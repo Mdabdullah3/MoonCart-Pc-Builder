934e2f30462a765210337a2bf2d418a6243aef45
Mdabdullah3 <mdabdullahnoman777@gmail.com>
2023-09-10 10:01:20 +0600

    reducer func done

--- src/pages/pcBuilder/index.js
+++ src/pages/pcBuilder/index.js
@@ -1,15 +1,18 @@
+/* eslint-disable @next/next/no-img-element */
 import RootLayout from '@/components/layout/RootLayout';
 import { categoriData } from '@/components/ui/CategoriData';
+import { removeFromPc } from '@/redux/features/pcBuilder/pcBuilderSlice';
 import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { RiDeleteBin6Line } from 'react-icons/ri';
+import { useDispatch, useSelector } from 'react-redux';
 
 const PcBuilder = () => {
     const categoriItems = categoriData
     const pcComponents = useSelector((state) => state?.pcBuilder?.pc);
     console.log(pcComponents);
-
+    const dispatch = useDispatch()
     return (
         <div className='py-40 bg-white'>
             <div className='pb-10 rounded-xl bg-white w-9/12 px-20 mx-auto border-2 border-gray-500'>
@@ -24,32 +27,52 @@ const PcBuilder = () => {
                                     <div className='flex items-center justify-between '>
                                         <div className='flex gap-4 items-center rounded-md'>
                                             <Image src={item?.icon} alt="" width={80} />
-                                            <h1 className='text-lg tracking-wider'>{item.name}</h1>
+                                            <h1 className='text-lg tracking-wider'>{item?.name}</h1>
                                         </div>
                                         <div>
-                                            <Link className='border-2 border-[#F4D94A] px-4 py-2 bg-[#F4D94A] text-white' href={`/pcBuilder/choose/${item.categor}`}>Choose</Link>
+                                            <Link className='border-2 border-[#F4D94A] px-4 py-2 bg-[#F4D94A] text-white' href={`/pcBuilder/choose/${item?.categor}`}>Choose</Link>
                                         </div>
                                     </div>
-                                    <div className='py-4 flex items-center gap-10 justify-center'>
-                                        <div>
-                                            <img className='w-20' src={pcComponents?.[item.categor]?.img} alt="" />
+                                    {
+                                        pcComponents?.[item?.categor]?.name && <div className='py-4 flex items-center gap-10 justify-evenly'>
+                                            <div>
+                                                {
+                                                    pcComponents?.[item?.categor]?.name && <img className='w-20' src={pcComponents?.[item.categor]?.img} alt="component" />
+                                                }
+                                            </div>
+                                            <div>
+                                                {
+                                                    pcComponents?.[item?.categor]?.name && <div>
+                                                        <h1>{pcComponents?.[item.categor]?.name}</h1>
+                                                    </div>
+                                                }
+                                            </div>
+                                            <div>
+                                                {
+                                                    pcComponents?.[item?.categor]?.name && <h1
+                                                        onClick={() => dispatch(removeFromPc({ category: item?.categor }))}
+                                                        className='text-xl cursor-pointer'><RiDeleteBin6Line /></h1>
+                                                }
+                                            </div>
                                         </div>
-                                        <div>
-                                            {
-                                                pcComponents?.[item?.categor]?.name && <div>
-                                                    <h1>{pcComponents?.[item.categor]?.name}</h1>
-                                                </div>
-                                            }
-                                        </div>
-                                    </div>
-                                </div>
+                                    }
+                                </div >
 
                             </>)
                         }
                     </div>
+                    {Object.keys(pcComponents).length === 7 &&
+                        Object.values(pcComponents).every(
+                            (component) => component?.name
+                        ) && <div className='block mx-auto text-center mt-8'>
+                            <button className='text-lg tracking-wider px-5 py-2 bg-[#F4D94A] text-white border-2 border-[#F4D94A] transition duration-300 hover:bg-transparent hover:text-black rounded-md'>
+                                Complete Build
+                            </button>
+                        </div>
+                    }
                 </div>
             </div>
-        </div>
+        </div >
     );
 };
 
